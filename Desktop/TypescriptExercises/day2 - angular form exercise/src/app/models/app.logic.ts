143b51826e1a74d4d6e478750441006f66f3de64
RajNayyar <rajpreetsingh0005@gmail.com>
2020-05-12 14:47:41 +0530

    updated default assignment

--- Desktop/TypescriptExercises/day2 - angular form exercise/src/app/models/app.logic.ts
+++ Desktop/TypescriptExercises/day2 - angular form exercise/src/app/models/app.logic.ts
@@ -21,42 +21,4 @@ export class Logic {
       this.products.push(prd);
       return this.products;
     }
-    searchProducts(searchText: string): Array<Product>
-    {
-      let searchResults: Array<Product> = []
-      this.products.forEach(element => {
-        if(element.Category.toLowerCase().includes(searchText.toLowerCase()) || element.ProductName.toLowerCase().includes(searchText.toLowerCase()))
-          {
-            searchResults.push(element)
-          }
-      });
-      return searchResults
-    }
-    fetchIndex(prd:Product): number
-    {
-      return this.products.map(function(e) { return e.ProductId }).indexOf(prd.ProductId);
-    }
-    updateProduct(prd:Product): Array<Product>{
-      let indexToUpdate = this.fetchIndex(prd)
-      if(indexToUpdate == -1)
-        {
-          alert("Invalid product Id")
-          return this.products
-        }
-      else
-      {
-        this.products[indexToUpdate].Category = prd.Category
-        this.products[indexToUpdate].Price = prd.Price
-        this.products[indexToUpdate].ProductId = prd.ProductId
-        this.products[indexToUpdate].ProductName = prd.ProductName
-      }
-      return this.products
-    }
-    deleteProducts(prd: Product): Array<Product>
-    {
-      debugger
-      let indexToDelete = this.products.map(function(e) { return e.ProductId }).indexOf(prd.ProductId);
-      this.products.splice(indexToDelete, 1)
-      return this.products
-    }
 }
