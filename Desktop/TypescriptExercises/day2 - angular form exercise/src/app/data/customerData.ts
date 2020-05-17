c021cf82222618f4b32e225317eb311c9248eec6
RajNayyar <rajpreetsingh0005@gmail.com>
2020-05-17 23:01:14 +0530

    day-5 assignment

---
+++ Desktop/TypescriptExercises/day2 - angular form exercise/src/app/data/customerData.ts
@@ -0,0 +1,9 @@
+export const customers = [
+    {ProductId:101, ProductName:'Laptop', Price:20000, Catgory:'ECT'},
+    {ProductId:102, ProductName:'Iron', Price:2000, Catgory:'ECL'},
+    {ProductId:103, ProductName:'Lays', Price:20, Catgory:'FOD'}
+  ];
+  
+  export const order =[
+    'ECT', 'ECL', 'FOD'
+  ];
